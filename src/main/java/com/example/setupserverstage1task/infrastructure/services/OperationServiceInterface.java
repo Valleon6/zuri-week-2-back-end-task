7f8bb0c7dca06b7ba896964acaf2a72c83a88ea5
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-03 23:17:46 +0100

    Create OperationServiceInterface.java

---
+++ src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationServiceInterface.java
@@ -0,0 +1,6 @@
+package com.example.setupserverstage1task.infrastructure.services;
+
+public interface OperationServiceInterface {
+
+    int operationImpl(OperationEnum operationEnum, int x, int y);
+}
