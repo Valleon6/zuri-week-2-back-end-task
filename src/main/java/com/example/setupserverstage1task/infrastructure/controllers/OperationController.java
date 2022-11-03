f5b6fa06594d85717c385cc56d14be73e132de34
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-03 23:18:39 +0100

    Create OperationController.java

---
+++ src/main/java/com/example/setupserverstage1task/infrastructure/controllers/OperationController.java
@@ -0,0 +1,30 @@
+package com.example.setupserverstage1task.infrastructure.controllers;
+
+import com.example.setupserverstage1task.infrastructure.services.OperationService;
+import com.example.setupserverstage1task.payload.request.OperationRequest;
+import com.example.setupserverstage1task.payload.response.OperationResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class OperationController {
+    private OperationService operationService;
+
+
+    @PostMapping(path = "/operation-calculation")
+    public ResponseEntity<OperationResponse> operationResult(@RequestBody OperationRequest operationRequest) throws Exception {
+
+        return new ResponseEntity<>(new OperationResponse(
+                "Valleon",
+                operationRequest.getOperationEnum(),
+                operationService.getResult() ),
+                HttpStatus.OK);
+
+    }
+
+}
