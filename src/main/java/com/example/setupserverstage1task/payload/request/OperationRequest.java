a2e8a7f9a435d8e49ad899efae77f18910c2dd2c
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-04 19:26:20 +0100

    Update OperationRequest.java

--- src/main/java/com/example/setupserverstage1task/payload/request/OperationRequest.java
+++ src/main/java/com/example/setupserverstage1task/payload/request/OperationRequest.java
@@ -1,15 +1,35 @@
 package com.example.setupserverstage1task.payload.request;
 
+import com.example.setupserverstage1task.domain.entities.OperationType;
 import com.example.setupserverstage1task.infrastructure.services.OperationEnum;
 import lombok.*;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class OperationRequest {
+
+    private OperationType operationType;
     private OperationEnum operationEnum;
     private int x;
     private int y;
 
+    public OperationRequest(OperationType operationType, int x, int y) {
+        this.operationType = operationType;
+        this.x = x;
+        this.y = y;
+    }
 
+    public OperationRequest(OperationEnum operationEnum, int x, int y) {
+        this.operationEnum = operationEnum;
+        this.x = x;
+        this.y = y;
+    }
+
+    public OperationRequest(OperationEnum operationEnum) {
+        this.operationEnum = operationEnum;
+    }
+
+    public OperationRequest(OperationType operationType) {
+        this.operationType = operationType;
+    }
 }
