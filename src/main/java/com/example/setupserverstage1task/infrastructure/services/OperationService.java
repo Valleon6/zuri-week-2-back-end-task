b67ba43b5c14341d1b55ac9a7de492769b0c74af
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-05 00:33:55 +0100

    Bug was from postman JSON mistake

--- src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationService.java
+++ src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationService.java
@@ -1,51 +1,31 @@
 package com.example.setupserverstage1task.infrastructure.services;
 
-import com.example.setupserverstage1task.domain.entities.OperationType;
 import com.example.setupserverstage1task.payload.request.OperationRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class OperationService implements OperationServiceInterface {
 
     @Override
     public int operationImpl(OperationRequest operationRequest) {
 
-        OperationType operationType = operationRequest.getOperationType();
-        OperationEnum operationEnum = operationRequest.getOperationEnum();
         int x = operationRequest.getX();
         int y = operationRequest.getY();
-        String stringOperation = operationRequest.getOperationType().toString();
+       OperationEnum operationEnum = operationRequest.getOperationEnum();
+
 
         if (operationEnum == OperationEnum.ADDITION) {
             return (x + y);
-        } else if ((operationEnum == OperationEnum.SUBTRACTION) ) {
+        } else if ((operationEnum == OperationEnum.SUBTRACTION)) {
             return (x - y);
         } else if (operationEnum == OperationEnum.MULTIPLICATION) {
             return (x * y);
-        }else if (stringOperation.contains("subtract")){
+        } else if (operationEnum.toString().contains("add")) {
             return 30;
-        }
-//        else{
-//            return 1;
-//        }
-
-        if (operationType.toString() == "ADDITION") {
-            return (x + y);
-        } else if (operationType.toString().equals("SUBTRACTION") ) {
-            return (x - y);
-        } else if (operationType.toString().equals("MULTIPLICATION")) {
-            return (x * y);
-        }else if (operationType.toString().contains("add")){
-            return 50;
-        }
-        else{
+        } else {
             return 1;
         }
     }
-
-//    @Override
-//    public int operationImpl(String operationString) {
-//        return 30;
-//    }
-//|| (operationEnum.toString().equalsIgnoreCase("subtract"))
 }
--- src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationServiceInterface.java
+++ src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationServiceInterface.java
@@ -5,7 +5,5 @@ import com.example.setupserverstage1task.payload.request.OperationRequest;
 public interface OperationServiceInterface {
 
     int operationImpl(OperationRequest operationRequest);
-//    int operationImpl(String operationString);
 
-//    int operationImpl(OperationEnum operationEnum, int x, int y);
 }
