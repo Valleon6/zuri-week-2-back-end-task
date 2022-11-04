9d89aa3ddd30bbd8faf321d1ed60af9b3640df80
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-04 19:26:31 +0100

    Update OperationServiceInterface.java

--- src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationServiceInterface.java
+++ src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationServiceInterface.java
@@ -1,6 +1,11 @@
 package com.example.setupserverstage1task.infrastructure.services;
 
+import com.example.setupserverstage1task.payload.request.OperationRequest;
+
 public interface OperationServiceInterface {
 
-    int operationImpl(OperationEnum operationEnum, int x, int y);
+    int operationImpl(OperationRequest operationRequest);
+//    int operationImpl(String operationString);
+
+//    int operationImpl(OperationEnum operationEnum, int x, int y);
 }
