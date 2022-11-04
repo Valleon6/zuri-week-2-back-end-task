1ab8acb0f8efb480730867502f2c0fac48928d2f
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-04 19:26:44 +0100

    Update OperationService.java

--- src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationService.java
+++ src/main/java/com/example/setupserverstage1task/infrastructure/services/OperationService.java
@@ -1,40 +1,51 @@
 package com.example.setupserverstage1task.infrastructure.services;
 
+import com.example.setupserverstage1task.domain.entities.OperationType;
 import com.example.setupserverstage1task.payload.request.OperationRequest;
-import com.example.setupserverstage1task.payload.response.OperationResponse;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-@Getter
-@Setter
-public class OperationService implements OperationServiceInterface{
-    public OperationRequest operationRequest;
-    private int result;
+public class OperationService implements OperationServiceInterface {
 
     @Override
-    public int operationImpl(OperationEnum operationEnum, int x, int y) {
+    public int operationImpl(OperationRequest operationRequest) {
 
-       operationEnum = operationRequest.getOperationEnum();
-           x =      operationRequest.getX();
-           y =     operationRequest.getY();
+        OperationType operationType = operationRequest.getOperationType();
+        OperationEnum operationEnum = operationRequest.getOperationEnum();
+        int x = operationRequest.getX();
+        int y = operationRequest.getY();
+        String stringOperation = operationRequest.getOperationType().toString();
 
-        if (operationEnum == OperationEnum.ADDITION){
-            result = x + y;
-
+        if (operationEnum == OperationEnum.ADDITION) {
+            return (x + y);
+        } else if ((operationEnum == OperationEnum.SUBTRACTION) ) {
+            return (x - y);
+        } else if (operationEnum == OperationEnum.MULTIPLICATION) {
+            return (x * y);
+        }else if (stringOperation.contains("subtract")){
+            return 30;
         }
-        else if(operationEnum == OperationEnum.SUBTRACTION){
-            result = x -y;
+//        else{
+//            return 1;
+//        }
 
+        if (operationType.toString() == "ADDITION") {
+            return (x + y);
+        } else if (operationType.toString().equals("SUBTRACTION") ) {
+            return (x - y);
+        } else if (operationType.toString().equals("MULTIPLICATION")) {
+            return (x * y);
+        }else if (operationType.toString().contains("add")){
+            return 50;
         }
-        else if(operationEnum == OperationEnum.MULTIPLICATION) {
-            result = x * y;
+        else{
+            return 1;
         }
-       return 17;
-
     }
 
-
+//    @Override
+//    public int operationImpl(String operationString) {
+//        return 30;
+//    }
+//|| (operationEnum.toString().equalsIgnoreCase("subtract"))
 }
